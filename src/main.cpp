fd68bf586a560789e837c64fea0e868bc99405e9
Chris Raabe <chris@raabe.nospam>
2014-05-29 13:45:04 +0900

    Added communications with VehicleWrapper. First working version in U of Tokyo's Vicon lab.

--- src/main.cpp
+++ src/main.cpp
@@ -30,7 +30,7 @@ struct PTAMPacket_t {
 	uint16_t checksum;
 } __attribute__((packed));
 
-// Definition of packet from PTAM
+// Definition of packet from vehicle wrapper
 struct VehiclePacket_t {
 	uint16_t motor_mask[2];
 	uint16_t checksum;
@@ -40,6 +40,14 @@ struct VehiclePacket_t {
 	int16_t roll;
 } __attribute__((packed));
 
+// Definition of health packet for vehicle wrapper
+struct HealthPacket_t {
+	uint8_t sync_char[2];
+	uint16_t timestamp;
+	uint32_t header;
+	uint16_t checksum;
+	uint16_t data[28];
+} __attribute__((packed));
 
 static void sigterm_handler(int sig)
 {
@@ -48,7 +56,7 @@ static void sigterm_handler(int sig)
 	if (received_nb_signals > 3) exit(123);
 }
 
-static int init()
+static int youbot_init()
 {
 	//check if base hardware exists
 	try
@@ -164,7 +172,7 @@ static bool parse_ptam_packet(PTAMPacket_t* const ptam_packet,
 	static union bus {
 		PTAMPacket_t ptam_packet;
 		uint8_t bytes[ptam_packet_size];
-	} rx_packet;
+	} rx;
 
 	static int num_bytes_stored = -1;  // -1 indicates header not yet received
 
@@ -172,16 +180,16 @@ static bool parse_ptam_packet(PTAMPacket_t* const ptam_packet,
 		&& (num_bytes_stored < ptam_packet_size); ++i)
 	{
 		if ((num_bytes_stored > 0) || (rx_buffer[i] == 0xE5))
-			rx_packet.bytes[num_bytes_stored++] = rx_buffer[i];
+			rx.bytes[num_bytes_stored++] = rx_buffer[i];
 	}
 
 	if (num_bytes_stored == ptam_packet_size)
 	{
 		num_bytes_stored = 0;
-		if (checksum(rx_packet.bytes, ptam_packet_size - 2)
-			== rx_packet.ptam_packet.checksum)
+		if (checksum(rx.bytes, ptam_packet_size - 2)
+			== rx.ptam_packet.checksum)
 		{
-			*ptam_packet = rx_packet.ptam_packet;
+			*ptam_packet = rx.ptam_packet;
 			return true;
 		}
 		else
@@ -200,7 +208,7 @@ static bool parse_vehicle_packet(VehiclePacket_t* const vehicle_packet,
 	static union bus {
 		VehiclePacket_t vehicle_packet;
 		uint8_t bytes[vehicle_packet_size];
-	} rx_packet;
+	} rx;
 
 	static int num_bytes_stored = -1;  // -1 indicates header not yet received
 	static uint8_t last_byte_received = 0;
@@ -217,46 +225,109 @@ static bool parse_vehicle_packet(VehiclePacket_t* const vehicle_packet,
 		else  // Header received
 		{
 			if (num_bytes_stored < vehicle_packet_size)
-				rx_packet.bytes[num_bytes_stored++] = rx_buffer[i];
+				rx.bytes[num_bytes_stored++] = rx_buffer[i];
 		}
 	}
 
 	if (num_bytes_stored == vehicle_packet_size)
 	{
-		// cout << "Collective: " << rx_packet.vehicle_packet.collective
-		// 	<< " Yaw:" << rx_packet.vehicle_packet.yaw
-		// 	<< " Pitch:" << rx_packet.vehicle_packet.pitch
-		// 	<< " Roll:" << rx_packet.vehicle_packet.roll << endl;
+		// cout << "Collective: " << rx.vehicle_packet.collective
+		// 	<< " Yaw:" << rx.vehicle_packet.yaw
+		// 	<< " Pitch:" << rx.vehicle_packet.pitch
+		// 	<< " Roll:" << rx.vehicle_packet.roll << endl;
 
 		num_bytes_stored = -1;  // Start looking for the header again
 		last_byte_received = 0;
 
-		convert_endienness(&rx_packet.vehicle_packet.motor_mask[0]);
-		convert_endienness(&rx_packet.vehicle_packet.motor_mask[1]);
-		convert_endienness(&rx_packet.vehicle_packet.checksum);
-		convert_endienness(&rx_packet.vehicle_packet.collective);
-		convert_endienness(&rx_packet.vehicle_packet.yaw);
-		convert_endienness(&rx_packet.vehicle_packet.pitch);
-		convert_endienness(&rx_packet.vehicle_packet.roll);
+		convert_endienness(&rx.vehicle_packet.motor_mask[0]);
+		convert_endienness(&rx.vehicle_packet.motor_mask[1]);
+		convert_endienness(&rx.vehicle_packet.checksum);
+		convert_endienness(&rx.vehicle_packet.collective);
+		convert_endienness(&rx.vehicle_packet.yaw);
+		convert_endienness(&rx.vehicle_packet.pitch);
+		convert_endienness(&rx.vehicle_packet.roll);
 
-		uint16_t computed_checksum = rx_packet.vehicle_packet.motor_mask[0]
-			+ rx_packet.vehicle_packet.motor_mask[1]
-			+ (uint16_t)(rx_packet.vehicle_packet.collective
-			+ rx_packet.vehicle_packet.yaw + rx_packet.vehicle_packet.pitch
-			+ rx_packet.vehicle_packet.roll);
-		if (rx_packet.vehicle_packet.checksum == computed_checksum)
+		uint16_t computed_checksum = rx.vehicle_packet.motor_mask[0]
+			+ rx.vehicle_packet.motor_mask[1]
+			+ (uint16_t)(rx.vehicle_packet.collective
+			+ rx.vehicle_packet.yaw + rx.vehicle_packet.pitch
+			+ rx.vehicle_packet.roll);
+		if (rx.vehicle_packet.checksum == computed_checksum)
 		{
-			*vehicle_packet = rx_packet.vehicle_packet;
+			*vehicle_packet = rx.vehicle_packet;
 			return true;
 		}
 		else
 		{
-			cout << "Checksum error: expected " << rx_packet.vehicle_packet.checksum
+			cout << "Checksum error: expected " << rx.vehicle_packet.checksum
 				<< " but computed " << computed_checksum << endl;
 		}
 	}
 
 	return false;
+}
+
+int form_health_packet(HealthPacket_t* const health_packet)
+{
+// current
+#define MASK_CURRENT1            0x00000001
+#define MASK_CURRENT2            0x00000002
+#define MASK_CURRENT3            0x00000004
+#define MASK_CURRENT4            0x00000008
+// temperature
+#define MASK_TEMP1               0x00000010
+#define MASK_TEMP2               0x00000020
+#define MASK_TEMP3               0x00000040
+#define MASK_TEMP4               0x00000080
+// gyros
+#define MASK_X_POS               0x00001000
+#define MASK_Y_POS               0x00002000
+// voltage
+#define MASK_MEASURED_VOLTAGE    0x00010000
+#define MASK_ESTIMATED_VOLTAGE   0x00040000
+// comm
+#define MASK_COMM_ERROR          0x00100000
+#define MASK_LATENCY             0x00200000
+#define MASK_TICKS_SINCE_LAST    0x00400000
+#define MASK_SETTINGS            0x00800000
+// motors
+#define MASK_DUTY_MOTOR1         0x01000000
+#define MASK_DUTY_MOTOR2         0x02000000
+#define MASK_DUTY_MOTOR3         0x04000000
+#define MASK_DUTY_MOTOR4         0x08000000
+// debug
+#define MASK_DEBUG_DATA1         0x10000000
+#define MASK_DEBUG_DATA2         0x20000000
+#define MASK_DEBUG_DATA3         0x40000000
+#define MASK_DEBUG_DATA4         0x80000000
+
+	static uint16_t counter = 0;
+
+	health_packet->sync_char[0] = '~';
+	health_packet->sync_char[1] = '~';
+	health_packet->timestamp = ++counter;
+	health_packet->header = MASK_MEASURED_VOLTAGE | MASK_ESTIMATED_VOLTAGE
+		| MASK_SETTINGS;
+	health_packet->data[0] = (uint16_t)(12.0 * 204.6);
+	health_packet->data[1] = (uint16_t)(12.0 * 204.6);
+	health_packet->data[2] = 0x0001;
+
+	health_packet->checksum = health_packet->timestamp
+		+ (uint16_t)((health_packet->header & 0xFFFF0000) >> 16)
+		+ (uint16_t)(health_packet->header & 0x0000FFFF)
+		+ health_packet->data[0] + health_packet->data[1] + health_packet->data[2];
+
+	convert_endienness(&health_packet->timestamp);
+	convert_endienness(&health_packet->header);
+	convert_endienness(&health_packet->checksum);
+	convert_endienness(&health_packet->data[0]);
+	convert_endienness(&health_packet->data[1]);
+	convert_endienness(&health_packet->data[2]);
+
+	// Determine length of: sync characters, timestamp, header, checksum & data
+	return sizeof(health_packet->sync_char) + sizeof(health_packet->timestamp)
+		+ sizeof(health_packet->header) + sizeof(health_packet->checksum)
+		+ 3 * sizeof(uint16_t);
 }
 
 void arm_position(const float position_array[5])
@@ -280,11 +351,11 @@ int main()
 	signal(SIGINT , sigterm_handler); /* Interrupt (ANSI).  */
 	signal(SIGTERM, sigterm_handler); /* Termination (ANSI).  */
 
-	init();
-
+	youbot_init();
+/*
 	if (!arm_exists)
 		return 1;
-
+*/
 	if (!base_exists)
 		return 1;
 
@@ -293,7 +364,7 @@ int main()
 	if (!serial.IsOpen())
 		return 1;
 
-	arm_position(arm_up);
+	// arm_position(arm_up);
 
 	// Starting the event loop
 	while (received_sigterm == 0)
@@ -320,17 +391,43 @@ int main()
 */
 
 		VehiclePacket_t vehicle_packet;
-		parse_vehicle_packet(&vehicle_packet, rx_buffer, bytes_received);
+		if (parse_vehicle_packet(&vehicle_packet, rx_buffer, bytes_received))
+		{
+
+			try
+			{
+				youbot_base->setBaseVelocity(
+					-0.75 * ((float)vehicle_packet.pitch / 1250. - 1.) * meter_per_second,
+					0.75 * ((float)vehicle_packet.roll / 1250. - 1.) * meter_per_second,
+					2.0 * ((float)vehicle_packet.yaw / 1250. - 1.) * radian_per_second);
+			}
+			catch(exception& ex)
+			{
+				cout << "BaseWHAT: " << ex.what() << endl;
+			}
+
+			cout << "pitch: " << -((float)vehicle_packet.pitch / 1250. - 1.)
+			<< ", roll: " << ((float)vehicle_packet.roll / 1250. - 1.)
+			<< ", yaw: " << ((float)vehicle_packet.yaw / 1250. - 1.) << endl;
+		}
+
+		const uint8_t health_packet_size = sizeof(HealthPacket_t);
+		static union bus {
+			HealthPacket_t health_packet;
+			uint8_t bytes[health_packet_size];
+		} tx;
+		int tx_length = form_health_packet(&tx.health_packet);
+		serial.SendBuffer(&tx.bytes[0], tx_length);
 
 		usleep(10000);
 	}
 
 	serial.Close();
 
-	cout << "Please wait while stowing arm." << endl;
-	arm_position(arm_down);
+	// cout << "Please wait while stowing arm." << endl;
+	// arm_position(arm_down);
 
-	SLEEP_MILLISEC(4000);
+	// SLEEP_MILLISEC(4000);
 
 	return 0;
 }
